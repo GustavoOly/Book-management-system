f1e8eb37906b98c10ee857d8103a229fa24bb1d9
Gustavo Oly <gustavooly.dev@gmail.com>
2024-07-22 08:45:54 -0300

    feat: pages

---
+++ src/renderer/src/pages/Historico.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import NavBar from "../components/NavBar"
+import HistoricTable from "../components/HistoricTable"
+
+
+export default function Historico() {
+    return (
+        <>
+            <Header />
+            <NavBar />
+            <HistoricTable />
+        </>
+
+    )
+}
---
+++ src/renderer/src/pages/Novo.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+import NavBar from "../components/NavBar"
+
+export default function Novo() {
+    return (
+        <>
+            <Header />
+            <NavBar />
+        </>
+    )
+}
