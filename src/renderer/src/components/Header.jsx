b423b424984bcde1a52037fd51f066861fa11d0f
Gustavo Oly <gustavooly.dev@gmail.com>
2024-08-03 17:11:00 -0300

    feat: props

--- src/renderer/src/components/Header.jsx
+++ src/renderer/src/components/Header.jsx
@@ -1,10 +1,10 @@
-export default function Header() {
+export default function Header({ titulo, nome }) {
     return (
         <header className="h-20 px-2 bg-primary text-secundary">
             <section className="w-full h-full max-w-7xl m-auto flex justify-center items-center">
                 <div>
-                    <h1 className="font-bold text-2xl bg-red-900">BIBLIOTECA MUNICIPAL</h1>
-                    <p className="text-center">JOÃO VEIGA</p>
+                    <h1 className="font-bold text-2xl bg-red-900">{titulo}</h1>
+                    <p className="text-center">{nome}</p>
                 </div>
             </section>
         </header>
