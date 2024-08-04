e643c4ef405c9a6a04d58f9df22b00cf190d2cfc
Gustavo Oly <gustavooly.dev@gmail.com>
2024-08-04 10:10:21 -0300

    config: database

--- database/database.js
+++ database/database.js
@@ -1,21 +1,22 @@
 import Database from 'better-sqlite3';
 import { resolve } from 'path';
 
-const dbPath = resolve('./database/database.db');
+const dbPath = resolve('./database.db');
 const db = new Database(dbPath);
 
 const createTable = `
   CREATE TABLE IF NOT EXISTS emprestimos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
+    nome TEXT NOT NULL,\
     telefone TEXT NOT NULL,
     livros TEXT NOT NULL,
     dataEmprestimo TEXT NOT NULL,
     dataDevolucao TEXT NOT NULL,
-    estado TEXT NOT NULL
+    estado TEXT
   )
 `;
 
 db.exec(createTable);
+console.log("Tabela 'emprestimos' criada/verificada com sucesso.");
 
 export default db;
